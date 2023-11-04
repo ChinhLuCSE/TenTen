bd24e47cc3b9abaf5caf28052a50f5a78d7eb930
ChinhLuCSE <chinhlucse@gmail.com>
2023-11-04 13:54:29 +0700

    improve-login-ui

--- src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
@@ -14,14 +14,16 @@ const SignInPage = () => {
   const [password, setPassword] = useState("");
   const [policyAccepted1, setPolicyAccepted1] = useState(false);
   const [policyAccepted2, setPolicyAccepted2] = useState(false);
+  const [phoneNumberModal, setPhoneNumberModal] = useState("");
+  const [otpNumberModal, setOtpNumberModal] = useState("");
+  const [newPasswordRetype, setNewPasswordRetype] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
   };
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
+
   const handleCancel = () => {
     setIsModalOpen(false);
   };
@@ -36,6 +38,8 @@ const SignInPage = () => {
     }
   };
 
+  const handleSubmitFormModal = () => {};
+
   return (
     <>
       <Header />
@@ -43,14 +47,17 @@ const SignInPage = () => {
         className={styles["body"]}
         style={{ marginTop: "30px", marginInline: "200px" }}
       >
-        <div>Chào mừng đến với LeaveTrack</div>
+        <div style={{ fontSize: "32px", fontWeight: "600" }}>
+          <span>Welcome to</span>{" "}
+          <span style={{ color: "#032A94" }}>LeaveTrack</span>{" "}
+        </div>
 
         <div className={styles["login-wrap"]} style={{ marginTop: "30px" }}>
           <div className={styles["left"]} style={{ width: "45%" }}>
             <form>
               <div style={{ display: "flex", flexDirection: "column" }}>
                 <label htmlFor="phoneNumber" className={styles["label"]}>
-                  Số điện thoại
+                  Phone number
                 </label>
                 <input
                   type="tel"
@@ -68,7 +75,7 @@ const SignInPage = () => {
                 }}
               >
                 <label htmlFor="password" className={styles["label"]}>
-                  Mật Khẩu
+                  Password
                 </label>
                 <input
                   type="password"
@@ -92,11 +99,18 @@ const SignInPage = () => {
                       checked={policyAccepted1}
                       onChange={() => setPolicyAccepted1(!policyAccepted1)}
                     />
+                    <span style={{ paddingLeft: "10px" }}>
+                      I agree with the{" "}
+                      <span style={{ color: "#293393", cursor: "pointer" }}>
+                        terms of service
+                      </span>{" "}
+                      and{" "}
+                      <span style={{ color: "#293393", cursor: "pointer" }}>
+                        privacy policy
+                      </span>{" "}
+                      of LeaveTrack.
+                    </span>
                   </label>
-                  <span style={{ paddingLeft: "10px" }}>
-                    Tôi đã đọc và đồng ý với điều khoản dịch vụ cũng như chính
-                    sách quyền riêng tư của LeaveTrack.
-                  </span>
                 </div>
                 <div>
                   <label>
@@ -105,10 +119,10 @@ const SignInPage = () => {
                       checked={policyAccepted2}
                       onChange={() => setPolicyAccepted2(!policyAccepted2)}
                     />
+                    <span style={{ paddingLeft: "10px" }}>
+                      I agree to receive news from LeaveTrack.
+                    </span>
                   </label>
-                  <span style={{ paddingLeft: "10px" }}>
-                    Tôi đồng ý nhận thông tin mới từ LeaveTrack
-                  </span>
                 </div>
               </div>
               <button
@@ -126,7 +140,7 @@ const SignInPage = () => {
                   marginTop: "10px",
                 }}
               >
-                <div>Đổi mật khẩu</div>
+                <div style={{ cursor: 'not-allowed' }}>Đổi mật khẩu</div>
                 <div
                   style={{ color: "#293393", cursor: "pointer" }}
                   onClick={showModal}
@@ -141,47 +155,78 @@ const SignInPage = () => {
           </div>
         </div>
       </div>
-      <Modal
-        title="Phục hồi mật khẩu"
-        open={isModalOpen}
-        onCancel={handleCancel}
-        footer={null}
-      >
-        <p>Nhập thông tin theo mẫu</p>
-        <form style={{ marginTop: "40px" }}>
-          <div>
-            <label htmlFor="phoneNumber" className={styles["label"]}>
-              Số điện thoại
-            </label>
-            <input
-              type="tel"
-              id="phoneNumber"
-              className={styles["input-box"]}
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="phoneNumber" className={styles["label"]}>
-              Mã OTP
-            </label>
-            <input
-              type="tel"
-              id="phoneNumber"
-              className={styles["input-box"]}
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-          </div>
-          <button
-            type="button"
-            className={styles["sign-in-button"]}
-            onClick={handleSignIn}
-            style={{ marginTop: "30px" }}
-          >
-            Tiếp tục
-          </button>
-        </form>
+      <Modal open={isModalOpen} onCancel={handleCancel} footer={null}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <p style={{ fontSize: "32px" }}>Phục hồi mật khẩu</p>
+          <p style={{ color: "rgba(0, 0, 0, 0.50)" }}>
+            Nhập thông tin theo mẫu
+          </p>
+          <form style={{ marginTop: "40px", marginInline: "40px" }}>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <label htmlFor="phoneNumber" className={styles["label"]}>
+                Phone number
+              </label>
+              <input
+                type="tel"
+                id="phoneNumberModal"
+                className={styles["input-box-2"]}
+                value={phoneNumberModal}
+                onChange={(e) => setPhoneNumberModal(e.target.value)}
+              />
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <label htmlFor="phoneNumber" className={styles["label"]}>
+                OTP code
+              </label>
+              <input
+                type="tel"
+                id="otpNumberModal"
+                className={styles["input-box-2"]}
+                value={otpNumberModal}
+                onChange={(e) => setOtpNumberModal(e.target.value)}
+              />
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <label htmlFor="phoneNumber" className={styles["label"]}>
+                New Password
+              </label>
+              <input
+                type="tel"
+                id="newPassword"
+                className={styles["input-box-2"]}
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <label htmlFor="phoneNumber" className={styles["label"]}>
+                Re-type new password
+              </label>
+              <input
+                type="tel"
+                id="newPasswordRetype"
+                className={styles["input-box-2"]}
+                value={newPasswordRetype}
+                onChange={(e) => setNewPasswordRetype(e.target.value)}
+              />
+            </div>
+            <button
+              type="button"
+              className={styles["sign-in-button"]}
+              onClick={handleSubmitFormModal}
+              style={{ marginTop: "30px" }}
+            >
+              Tiếp tục
+            </button>
+          </form>
+        </div>
       </Modal>
     </>
   );
