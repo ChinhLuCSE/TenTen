4c77f246256b2ac30465e5f95360d9af8936a8f8
nguyennghia0108 <102567731+NguyenNghia0108@users.noreply.github.com>
2023-11-04 13:53:07 +0700

    update header

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -9,19 +9,21 @@ const Header = () => {
           <Image src="/logo.png" width={128} height={32} alt="Logo" />
         </Link>
         <div className="flex items-center">
-          <h1 className="text-2xl font-medium">Hệ thống quản lý nghỉ phép</h1>
+          <h1 className="text-2xl font-medium">Employee Leave Management System</h1>
         </div>
         <div className="flex items-center space-x-1">
           <ul className="hidden space-x-2 md:inline-flex">
             <li>
-              <Link href="/" className="px-4 py-2 font-semibold text-gray-600 rounded">
-                Noti
-              </Link>
+              <div class="relative mr-4">
+                <Image className="mt-3" src="/notify.svg" width={24} height={24} alt="Logo" />
+                <span class="-top-2 absolute bg-red-600 text-white text-xs  mr-2 px-1 py-0.5 rounded-full dark:bg-red-900">101</span>
+              </div>
             </li>
             <li>
-              <Link href="/about" className="px-4 py-2 font-semibold text-gray-600 rounded">
-                Avatar
-              </Link>
+              <div class="relative">
+                <Image className="rounded-full" src="/avatar.svg" width={48} height={48} alt="Logo" />
+                <span class="-bottom-1 left-8 absolute w-3.5 h-3.5 bg-green-400  dark:border-gray-800 rounded-full"></span>
+              </div>
             </li>
           </ul>
         </div>
