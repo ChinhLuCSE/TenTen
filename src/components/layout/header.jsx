c1e74c8261803cd5c39870374453e340b71bfb5e
nguyennghia0108 <102567731+NguyenNghia0108@users.noreply.github.com>
2023-11-04 11:10:07 +0700

    update header

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,7 +1,16 @@
-
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
-    return <div className="header-container"></div>;
-}
- 
+  return (
+    <div className="header-container sticky top-0 z-30 w-full p-5 bg-white sm:px-7 shadow-md">
+      <div className="flex items-center justify-between max-w-7xl">
+        <Link href="/">
+          <Image src="/logo.png" width={128} height={32} alt="Logo" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 export default Header;
