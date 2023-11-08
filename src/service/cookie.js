8b98215545e3c29a899f6def4bef9f69c9c260e9
nguyennghia0108 <102567731+NguyenNghia0108@users.noreply.github.com>
2023-11-09 02:25:01 +0700

    update user

--- src/service/cookie.js
+++ src/service/cookie.js
@@ -1,1 +1,1 @@
-export const token = document.cookie.split('; ').find((row) => row.startsWith('token=')).split('=')[1];
+//export const token = document.cookie.split('; ').find((row) => row.startsWith('token=')).split('=')[1];
