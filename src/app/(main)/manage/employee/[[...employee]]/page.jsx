450120583adaaba2a874b3e17587a5ce933fa49e
nguyennghia0108 <102567731+NguyenNghia0108@users.noreply.github.com>
2023-11-10 20:37:36 +0700

    update leave management

--- src/app/(main)/manage/employee/[[...employee]]/page.jsx
+++ src/app/(main)/manage/employee/[[...employee]]/page.jsx
@@ -11,13 +11,15 @@ import UserImage from "@/assets/images/image_user.png";
 
 import { Space, Modal } from "antd";
 
-
 const data = [
   {
     stt: "1",
     id: "SS 2313212321",
     name: "Vo Cong Thanh",
     department: "Human resource",
+    gender:"Male",
+    birthDate: "23/02/1990",
+    role: "Employee",
     leaves: "10",
     starting_date: "23/02/2000",
     address: "23 Ly Thuong Kiet",
@@ -28,6 +30,9 @@ const data = [
     id: "SS 2313222321",
     name: "Vo Cong Thanh",
     department: "Human resource",
+    gender:"Male",
+    birthDate: "23/02/1990",
+    role: "Employee",
     leaves: "10",
     starting_date: "23/02/2000",
     address: "23 Ly Thuong Kiet",
@@ -38,6 +43,9 @@ const data = [
     id: "SS 2313211321",
     name: "Vo Cong Thanh",
     department: "Human resource",
+    gender:"Male",
+    birthDate: "23/02/1990",
+    role: "Employee",
     leaves: "10",
     starting_date: "23/02/2000",
     address: "23 Ly Thuong Kiet",
@@ -102,7 +110,7 @@ const EmployeeManagement = () => {
           <button
             type="button"
             className="items-center text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1 inline-flex text-center "
-            onClick={showModal}
+            onClick={() => showUpdateModal(record.id)}
           >
             Update
           </button>
@@ -110,17 +118,13 @@ const EmployeeManagement = () => {
       ),
     },
   ];
-
-  const [name, setName] = useState("Vo Cong Thanh");
-  const [gender, setGender] = useState("Male");
-  const [birthDate, setBirthDate] = useState("29/2/2002");
-  const [address, setAddress] = useState("23 Ly Thuong Kiet street, Thu Duc district, Ho Chi Minh city");
-  const [role, setRole] = useState("Admin");
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isRemoveModalOpen, setIsRemoveModalOpen] = useState(false);
 
-  const showModal = () => {
+  const showUpdateModal = (id) => {
+    setSelectedEmployee(data.find(employee => employee.id === id));
     setIsModalOpen(true);
   };
 
@@ -147,98 +151,97 @@ const EmployeeManagement = () => {
       </div>
       <Modal open={isModalOpen} onCancel={handleCancel} footer={null} width={1128} centered>
         <div className="flex flex-col px-9 pb-9 items-center justify-center">
-        <div className="flex w-full flex-col items-center p-2 mt-8">
-          <Image className="rounded-full" src={UserImage} width={128} height={32} alt="avatar"></Image>
-          <span className="font-bold ">{name}</span>
-          <span className="font-semibold text-gray-600">Human resources department</span>
-          <span className="text-gray-600">ID: 1234567890</span>
-        </div>
-        <div className="mt-6 w-full items-center pt-6 pb-8 px-16 shadow-2xl rounded-md">
-          <h1 className="text-2xl my-4 font-medium">Information</h1>
-          <div className="pt-5">
-            <form>
-            <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
-                <label className="inline-block w-1/4 text-left text-gray-600">Full Name</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Name"
-                  className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-              <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
-                <label className="inline-block w-1/4 text-left text-gray-600">Gender</label>
-                <input
-                  type="text"
-                  id="gender"
-                  name="gender"
-                  placeholder="Enter your gender"
-                  className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
-                  value={gender}
-                  onChange={(e) => setGender(e.target.value)}
-                />
-              </div>
-              <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
-                <label className="inline-block w-1/4 text-left text-gray-600">Birth of Date</label>
-                <input
-                  type="text"
-                  id="birthDate"
-                  name="birthDate"
-                  placeholder="Enter your birth date"
-                  className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
-                  value={birthDate}
-                  onChange={(e) => setBirthDate(e.target.value)}
-                />
-              </div>
-              <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
-                <label className="inline-block w-1/4 text-left text-gray-600">Address</label>
-                <input
-                  type="text"
-                  id="address"
-                  name="address"
-                  placeholder="Enter your address"
-                  className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
-                  value={address}
-                  onChange={(e) => setAddress(e.target.value)}
-                  
-                />
-              </div>
-              <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
-                <label className="inline-block w-1/4 text-left text-gray-600">Role</label>
-                <input
-                  type="text"
-                  id="role"
-                  name="role"
-                  placeholder="Enter your role"
-                  className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
-                  value={role}
-                  onChange={(e) => setRole(e.target.value)}
-                />
-              </div>
-              <div className="flex flex-row items-center justify-center">
-                <button
-                  type="button"
-                  className="mx-2 items-center text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 pt-3 pb-2.5 inline-flex text-center "
-                  onClick={handleCancel}
-                >
-                  <Image className="mr-2" src="/cancel.svg" width={20} height={20} alt="Logo" />
-                  Cancel
-                </button>
-                <button
-                  type="button"
-                  className="mx-2 items-center text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 pt-3 pb-2.5 inline-flex text-center "
-                  onClick={handleCancel}
-                >
-                  <Image className="mr-2" src="/submit.svg" width={20} height={20} alt="Logo" />
-                  Submit
-                </button>
-              </div>
-            </form>
+          <div className="flex w-full flex-col items-center p-2 mt-8">
+            <Image className="rounded-full" src={UserImage} width={128} height={32} alt="avatar"></Image>
+            <span className="font-bold ">{selectedEmployee ? selectedEmployee.name : ''}</span>
+            <span className="font-semibold text-gray-600">Human resources department</span>
+            <span className="text-gray-600">ID: 1234567890</span>
           </div>
-        </div>
+          <div className="mt-6 w-full items-center pt-6 pb-8 px-16 shadow-2xl rounded-md">
+            <h1 className="text-2xl my-4 font-medium">Information</h1>
+            <div className="pt-5">
+              <form>
+                <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
+                  <label className="inline-block w-1/4 text-left text-gray-600">Full Name</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Name"
+                    className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
+                    value={selectedEmployee ? selectedEmployee.name : ''}
+                    onChange={(e) => setSelectedEmployee({ ...selectedEmployee, name: e.target.value })}
+                  />
+                </div>
+                <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
+                  <label className="inline-block w-1/4 text-left text-gray-600">Gender</label>
+                  <input
+                    type="text"
+                    id="gender"
+                    name="gender"
+                    placeholder="Enter your gender"
+                    className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
+                    value={selectedEmployee ? selectedEmployee.gender : ''}
+                    onChange={(e) => setSelectedEmployee({ ...selectedEmployee, gender: e.target.value })}
+                  />
+                </div>
+                <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
+                  <label className="inline-block w-1/4 text-left text-gray-600">Birth of Date</label>
+                  <input
+                    type="text"
+                    id="birthDate"
+                    name="birthDate"
+                    placeholder="Enter your birth date"
+                    className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
+                    value={selectedEmployee ? selectedEmployee.birthDate : ''}
+    onChange={(e) => setSelectedEmployee({ ...selectedEmployee, birthDate: e.target.value })}
+                  />
+                </div>
+                <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
+                  <label className="inline-block w-1/4 text-left text-gray-600">Address</label>
+                  <input
+                    type="text"
+                    id="address"
+                    name="address"
+                    placeholder="Enter your address"
+                    className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
+                    value={selectedEmployee ? selectedEmployee.address : ''}
+    onChange={(e) => setSelectedEmployee({ ...selectedEmployee, address: e.target.value })}
+                  />
+                </div>
+                <div className="relative z-0 w-full mb-6 group border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600">
+                  <label className="inline-block w-1/4 text-left text-gray-600">Role</label>
+                  <input
+                    type="text"
+                    id="role"
+                    name="role"
+                    placeholder="Enter your role"
+                    className="flex-1 py-2 outline-none w-3/4 font-semibold text-gray-500"
+                    value={selectedEmployee ? selectedEmployee.role : ''}
+                    disabled="true"
+                  />
+                </div>
+                <div className="flex flex-row items-center justify-center">
+                  <button
+                    type="button"
+                    className="mx-2 items-center text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 pt-3 pb-2.5 inline-flex text-center "
+                    onClick={handleCancel}
+                  >
+                    <Image className="mr-2" src="/cancel.svg" width={20} height={20} alt="Logo" />
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    className="mx-2 items-center text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 pt-3 pb-2.5 inline-flex text-center "
+                    onClick={handleCancel}
+                  >
+                    <Image className="mr-2" src="/submit.svg" width={20} height={20} alt="Logo" />
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </Modal>
       <Modal open={isRemoveModalOpen} onCancel={handleCancelRemove} footer={null} centered>
@@ -246,21 +249,21 @@ const EmployeeManagement = () => {
           <Image src="/warn.svg" width={80} height={80} alt="Logo" />
           <h1 className="text-xl my-4 font-medium">You have decided to delete this employee?</h1>
           <div className="flex w-full flex-row items-center justify-center">
-                <button
-                  type="button"
-                  className="mx-4 border-solid border-2 items-center bg-white-900 hover:text-white hover:bg-red-700 hover:border-transparent focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-6 py-2 inline-flex text-center "
-                  onClick={handleCancelRemove}
-                >
-                  Cancel
-                </button>
-                <button
-                  type="button"
-                  className="mx-4 border-2 border-transparent items-center text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-6 py-2 inline-flex text-center "
-                  onClick={handleCancelRemove}
-                >
-                  Submit
-                </button>
-              </div>
+            <button
+              type="button"
+              className="mx-4 border-solid border-2 items-center bg-white-900 hover:text-white hover:bg-red-700 hover:border-transparent focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-6 py-2 inline-flex text-center "
+              onClick={handleCancelRemove}
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              className="mx-4 border-2 border-transparent items-center text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-6 py-2 inline-flex text-center "
+              onClick={handleCancelRemove}
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </Modal>
     </div>
--- src/app/(main)/manage/leave/[[...leave]]/page.jsx
+++ src/app/(main)/manage/leave/[[...leave]]/page.jsx
@@ -1,5 +1,128 @@
+"use client";
+
+import Header from "@/components/layout/header";
+import Sidebar from "@/components/layout/sidebar";
+import UserTable from "@/components/table/usertable";
+
+import { Space, Modal } from "antd";
+
+const data = [
+  {
+    id: "SS 2313212321",
+    status: "Approved",
+    name: "Vo Cong Thanh",
+    remaining_day: "10",
+    start_date: "23/02/2000",
+    end_date: "23/02/2000",
+    reason: "toi benh",
+  },
+  {
+    id: "SS 2313212321",
+    status: "Approved",
+    name: "Vo Cong Thanh",
+    remaining_day: "10",
+    start_date: "23/02/2000",
+    end_date: "23/02/2000",
+    reason: "toi benh",
+  },
+  {
+    id: "SS 2313212321",
+    status: "Pending",
+    name: "Vo Cong Thanh",
+    remaining_day: "10",
+    start_date: "23/02/2000",
+    end_date: "23/02/2000",
+    reason: "toi benh",
+  },
+  {
+    id: "SS 2313212321",
+    status: "Pending",
+    name: "Vo Cong Thanh",
+    remaining_day: "10",
+    start_date: "23/02/2000",
+    end_date: "23/02/2000",
+    reason: "toi benh",
+  },
+];
+
 const LeaveManagement = () => {
-    return <div>LeaveManagement Page For Admin</div>;
-}
- 
+  const columns = [
+    {
+      title: "#",
+      dataIndex: "rowKey",
+      render: (id, record, index) => { ++index; return index; },
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+    },
+    {
+      title: "Full name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Remaining days off",
+      dataIndex: "remaining_day",
+      key: "remaining_day",
+    },
+    {
+      title: "Start date",
+      dataIndex: "start_date",
+      key: "start_date",
+    },
+    {
+      title: "End date",
+      dataIndex: "end_date",
+      key: "end_date",
+    },
+    {
+      title: "Reason",
+      dataIndex: "reason",
+      key: "reason",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <button
+            type="button"
+            className="items-center text-white bg-red-700 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1 inline-flex text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+          >
+            Remove
+          </button>
+          {record.status === "Approved" && (
+          <button
+            type="button"
+            className="items-center text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-3 py-1 inline-flex text-center "
+          >
+            Approve
+          </button>
+        )}
+        </Space>
+      ),
+    },
+  ];
+  return (
+    <div>
+      <Header status={1}></Header>
+      <div className="flex flex-row">
+        <Sidebar />
+        <div className="flex flex-col mx-auto justify-center text-center">
+        <h1 className="mt-10 text-2xl font-medium">Leave application is pending approval</h1>
+        <div className="flex rounded-lg p-6 shadow-lg items-center">
+          <UserTable columns={columns} data={data.filter(item => item.status === "Approved")} />
+        </div>
+        <h1 className="mt-12 mb-6 text-2xl font-medium">Leave application approved</h1>
+        <div className="flex rounded-lg p-6 shadow-lg items-center">
+          <UserTable columns={columns} data={data.filter(item => item.status === "Pending")} />
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default LeaveManagement;
