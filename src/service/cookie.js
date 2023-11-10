066b63f92413ebb09e40f0a22687862a181607ce
ChinhLuCSE <chinhlucse@gmail.com>
2023-11-10 21:27:43 +0700

    comment

--- src/service/cookie.js
+++ src/service/cookie.js
@@ -1,6 +1,6 @@
-export const token = document.cookie
-  ? document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("token="))
-      .split("=")[1]
-  : "none";
+// export const token = document.cookie
+//   ? document.cookie
+//       .split("; ")
+//       .find((row) => row.startsWith("token="))
+//       .split("=")[1]
+//   : "none";
